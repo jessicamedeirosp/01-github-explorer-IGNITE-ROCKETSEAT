890f86dc9805cb2fda6192056c39b4b659994bbf
Jessica Medeiros <jessicamedeirosp96@yahoo.com.br>
2021-04-05 22:30:08 -0300

    creating component RepositoryList

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,33 @@
+export function RepositoryList() {
+    return (
+        <section class="repository-list">
+            <h1>Lista de repositórios</h1>
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="#">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="#">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="#">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="#">Acessar repositório</a>
+                </li>
+            </ul>
+        </section>
+    )
+}
