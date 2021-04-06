161dc8df46498fd476d0e0aefca4cc08fd5b36dc
Jessica Medeiros <jessicamedeirosp96@yahoo.com.br>
2021-04-06 08:16:51 -0300

    concept of properties

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,33 +1,18 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: "unform",
+  description: "Form in React",
+  link: "https://github.com/unform/unform",
+};
 export function RepositoryList() {
-    return (
-        <section class="repository-list">
-            <h1>Lista de repositórios</h1>
-            <ul>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="#">Acessar repositório</a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="#">Acessar repositório</a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="#">Acessar repositório</a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="#">Acessar repositório</a>
-                </li>
-            </ul>
-        </section>
-    )
+  return (
+    <section class="repository-list">
+      <h1>Lista de repositórios</h1>
+      <ul>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+      </ul>
+    </section>
+  );
 }
