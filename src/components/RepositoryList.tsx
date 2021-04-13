e08c2849acaa58586295a5fba566f42a52550ff3
Jessica Medeiros <jessicamedeirosp96@yahoo.com.br>
2021-04-13 09:40:14 -0300

    changing jsx files to tsx

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,9 +2,14 @@ import { useState, useEffect } from "react";
 
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
+interface Repository {
+  name: string,
+  description: string,
+  html_url: string,
+}
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
   
   // executa quando algo acontecer
   // primeiro parametro uma função
@@ -18,7 +23,7 @@ export function RepositoryList() {
   }, []);
 
   return (
-    <section class="repository-list">
+    <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
         {repositories.map((repository) => (
